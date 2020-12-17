11423d1ed44e14d0de18e5866455b3a11067962b
RavianXReaver <raveenshenal@gmail.com>
2020-12-17 13:04:55 +0530

    Added Dockerfile

--- Models/Provider.cs
+++ Models/Provider.cs
@@ -1,10 +1,3 @@
-using System;
-using System.Linq;
-using System.Threading.Tasks;
-using Discord;
-using Discord.Commands;
-using Discord.WebSocket;
-
 public class Provider
 {
     public string Name { get; set; }
